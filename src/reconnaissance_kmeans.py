d6ce1909cc10ff4c0fcbc17ba6da1d3dd9d77e4b
Sacha Carnière <sacha.carniere@etu.univ-cotedazur.fr>
2019-05-10 12:09:00 +0200

    ça marche pas

--- src/reconnaissance_kmeans.py
+++ src/reconnaissance_kmeans.py
@@ -5,10 +5,11 @@ from python_speech_features import logfbank
 import shutil
 from sklearn.cluster import KMeans
 import numpy as np
+import pickle
 
 verbose = True
+
 k1 = int(argv[1])
-k2 = int(argv[2])
 
 image = cv2.imread('../resources/base_samples/citron1.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -25,11 +26,10 @@ print('nb. of keypoints: ', len(keypoints))
 
 lesSIFT = []
 
-
 dimImg = []  # nb of mfcc per file
 listImg = glob.glob("../resources/base_samples/*.jpg")
 
-'''for img in listImg:
+for img in listImg:
     if verbose:
         print("###", img, "###")
     image = cv2.imread(img)
@@ -37,38 +37,29 @@ listImg = glob.glob("../resources/base_samples/*.jpg")
     sift = cv2.xfeatures2d.SIFT_create()
     keypoints, descriptors = sift.detectAndCompute(gray, None)
     if verbose:
-        print("MFCC: ", mfcc_feat.shape)
+        print("SIFT: ", descriptors)
     dimImg.append(len(keypoints))
-    lesSIFT = np.append(lesSIFT, mfcc_feat, axis=0)
+    lesSIFT = np.append(lesSIFT, descriptors, axis=0)
 
 try:
     with open("../resources/kmeans1.txt", "rb") as input:
         kmeans1 = pickle.load(input)
 except EOFError:
-    exit(-1)
+    kmeans1 = KMeans(n_clusters=k1, random_state=0).fit(lesSIFT)
+    with open("../resources/kmeans1.txt", "wb") as output:
+        pickle.dump(kmeans1, output, pickle.HIGHEST_PROTOCOL)
 
 bows = np.empty(shape=(0, k1), dtype=int)
 
-kmeans1new = kmeans1.predict(lesMfcc)
-
 i = 0
-for nb in dimSons:  # for each sound (file)
+for nb in dimImg:  # for each sound (file)
     tmpBow = [0] * k1
     j = 0
     while j < nb:  # for each MFCC of this sound (file)
-        tmpBow[kmeans1new[i]] += 1
+        tmpBow[kmeans1[i]] += 1
         j += 1
         i += 1
     copyBow = tmpBow.copy()
     bows = np.append(bows, [copyBow], 0)
 
 print(bows)
-
-try:
-    with open("../resources/kmeans2.txt", "rb") as input:
-        kmeans2 = pickle.load(input)
-except EOFError:
-    exit(-1)
-
-
-print(kmeans2.predict(bows))'''
